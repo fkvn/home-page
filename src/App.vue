ed6c752eb17fafc4a1448e7b7779d10042eb7ccc
fkvn <phucaone@gmail.com>
2020-02-02 16:46:40 -0800

    Created Navbar Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navbar />
+    <!-- <HelloWorld msg="Welcome to Your V/ue.js App of Kevin Ngo" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar";
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Navbar
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Helvetica, Avenir, Arial, sans-serif";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-size: 1.2em;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <b-navbar toggleable="lg">
+    <b-navbar-brand href="#"
+      ><img fluid-grow alt="Vue logo" src="../assets/logo.png"
+    /></b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">About</b-nav-item>
+        <b-nav-item href="#">Porfolio</b-nav-item>
+        <b-nav-item href="#">Projects</b-nav-item>
+        <b-nav-item href="#">Articles</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-input-group>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button size="sm" class="my-0 ml-1" type="submit"
+                >Search
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {}
+};
+</script>
+
+<style scoped>
+.navbar-light {
+  background-color: #222930;
+}
+.navbar-light .navbar-brand img {
+  width: 3em;
+}
+
+.navbar-light .navbar-nav .nav-link {
+  margin-right: 3em;
+  color: white;
+}
+
+.navbar-light .navbar-toggler {
+  background-color: white;
+  font-size: 0.6em;
+}
+</style>
